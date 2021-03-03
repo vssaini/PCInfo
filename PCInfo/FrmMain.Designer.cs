81dccf9de509bd3567d9609ab88ff32a0fbaa15f
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 07:05:39 +0530

    Add project files.

---
+++ PCInfo/FrmMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PCInfo
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDetails = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This computer details:";
+            // 
+            // txtDetails
+            // 
+            this.txtDetails.BackColor = System.Drawing.Color.White;
+            this.txtDetails.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDetails.ForeColor = System.Drawing.Color.Navy;
+            this.txtDetails.Location = new System.Drawing.Point(15, 50);
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtDetails.Size = new System.Drawing.Size(453, 185);
+            this.txtDetails.TabIndex = 2;
+            this.txtDetails.Text = "";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(480, 259);
+            this.Controls.Add(this.txtDetails);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PC Info";
+            this.Shown += new System.EventHandler(this.FrmMain_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtDetails;
+    }
+}
+
---
+++ PCInfo/FrmMain.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Net;
+using System.Net.Configuration;
+using System.Text;
+using System.Windows.Forms;
+using PCInfo.Properties;
+
+namespace PCInfo
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Shown(object sender, EventArgs e)
+        {
+            txtDetails.Clear();
+
+            var builder = new StringBuilder(string.Format(Resources.HostName, Dns.GetHostName()));
+            builder.Append(Environment.NewLine);
+
+            builder.Append(string.Format(Resources.MachineName, Environment.MachineName));
+            builder.Append(Environment.NewLine);
+
+            builder.Append(string.Format(Resources.UserDomainName, Environment.UserDomainName));
+            builder.Append(Environment.NewLine);
+
+            // Forest and Domains
+            var forest = Forest.GetCurrentForest();
+            builder.Append(Environment.NewLine);
+            builder.Append(string.Format(Resources.Forest, forest.Name)).Append(Environment.NewLine);
+
+            var domains = forest.Domains;
+            builder.Append(Environment.NewLine).Append(Resources.ForestDomains).Append(Environment.NewLine);
+            foreach (Domain domain in domains)
+            {
+                builder.Append(domain.Name);
+                builder.Append(", ");
+            }
+            builder.Append(Environment.NewLine);
+
+            // Domain related
+            var currDomain = Domain.GetCurrentDomain();
+
+            builder.Append(Environment.NewLine);
+            builder.Append(string.Format(Resources.DomainName, currDomain.Name));
+            builder.Append(Environment.NewLine);
+
+            using (var context = new PrincipalContext(ContextType.Domain))
+            {
+                var server = context.ConnectedServer; // "dc1.domain.com"
+                var splitted = server.Split('.'); // { "dc1", "domain", "com" }
+                var formatted = splitted.Select(s => string.Format("DC={0}", s)); // { "DC=dc1", "DC=domain", "DC=com" }
+                string.Join(",", formatted);
+
+                builder.Append(string.Format(Resources.CurrentController, server)).Append(Environment.NewLine);
+            }
+
+            // Domain Controllers
+            builder.Append(Environment.NewLine).Append(Resources.DC).Append(Environment.NewLine);
+
+            var dcs = GetDomainControllers(currDomain.GetDirectoryEntry());
+            foreach (var dc in dcs)
+            {
+                builder.Append(dc);
+                builder.Append(", ");
+            }
+
+            // All details in textbox
+            txtDetails.Text = builder.ToString().TrimEnd(", ".ToCharArray());
+        }
+
+        /// <summary>
+        /// Get dictionary of domain controllers with their readable or writable status.
+        /// </summary>
+        /// <param name="domainEntry">The domain entry representing domain.</param>
+        /// <returns>Return dictionary object</returns>
+        private static List<string> GetDomainControllers(DirectoryEntry domainEntry)
+        {
+            const string aName = "dNSHostName";
+            const string aPrimGroupId = "primaryGroupID";
+            var dcs = new List<string>();
+
+            // Search all DCs (both readable (521) and writable (516)
+            var searcher = new DirectorySearcher(domainEntry)
+            {
+                Filter = "(&(objectCategory=computer)(objectClass=computer)(|((primaryGroupID=516)(primaryGroupID=521))))"
+            };
+            searcher.PropertiesToLoad.AddRange(new[] { aName,aPrimGroupId });
+            var results = searcher.FindAll();
+
+            foreach (SearchResult result in results)
+            {
+                // To see all attribute values in Output window
+                //int counter = 1;
+                //DirectoryEntry entry = result.GetDirectoryEntry();
+                //foreach (PropertyValueCollection property in entry.Properties)
+                //    foreach (var value in from object o in property select o.ToString())
+                //    {
+                //        var info = string.Format("{0}. Property: {1} and Value: {2}", counter,property.PropertyName, value);
+                //        Debug.WriteLine(info);
+                //        counter++;
+                //    }
+
+                var name = Convert.ToString(result.Properties[aName][0]);
+                var pgId = Convert.ToString(result.Properties[aPrimGroupId][0]);
+
+                int primaryGroupID;
+                if (int.TryParse(pgId, out primaryGroupID))
+                {
+                    // RID for the "Read-only Domain Controllers" built-in group in Active Directory
+                    // Writable Domain Controllers have primaryGroupID set to 516 (the "Domain Controllers" group).
+                    name = string.Format(primaryGroupID == 521 ? "{0} (Read only)" : "{0}", name);
+                }
+
+                dcs.Add(name);
+            }
+
+            return dcs;
+        }
+    }
+}
